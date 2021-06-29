930518af8169be3aa6270c96f61bca609d2c2574
Luisa-3 <luisawill837@gmail.com>
2021-06-29 21:51:52 +0200

    Klasse Spiel um paar Dinge ergänzz

--- SPIEL.java
+++ SPIEL.java
@@ -1,13 +1,20 @@
 
 import java.awt.*;
 
-public class SPIEL
+public class SPIEL extends SIMULATION 
 {
     /** Spielfelder erstellen*/
     SPIELFELD Spielfeld1;
     SPIELFELD Spielfeld2;
     SPIELFELD Spielfeld3;
     SPIELFELD Spielfeld4;
+    SCHIFFEFÜNF schlachtschiff;
+    SCHIFFEVIER kreuzer;
+    SCHIFFEDREI zerstörer;
+    SCHIFFEDREI zerstörer1;
+    SCHIFFEDREI zerstörer2;
+    SCHIFFEZWEI uboot;
+    SCHIFFEZWEI uboot1;
     
     /** Konstruktor */
     SPIEL ()
@@ -16,6 +23,13 @@ public class SPIEL
         Spielfeld2 = new SPIELFELD();
         Spielfeld3 = new SPIELFELD();
         Spielfeld4 = new SPIELFELD();
+        schlachtschiff = new SCHIFFEFÜNF();
+        kreuzer = new SCHIFFEVIER();
+        zerstörer = new SCHIFFEDREI();
+        zerstörer1 = new SCHIFFEDREI();
+        zerstörer2 = new SCHIFFEDREI();
+        uboot = new SCHIFFEZWEI();
+        uboot1 = new SCHIFFEZWEI();
     }
 
 }
---
+++ UI_QUIZ.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class UI_QUIZ
+{
+    Frame fenster;
+    Label fragetext;
+    
+    
+
+    /**
+     * Constructor for objects of class OBERFLAECHE
+     */
+    public UI_QUIZ()
+    {
+        fenster = new Frame("UI");
+        fenster.setSize(600,400);
+        fenster.setLocation(0,30);
+        fenster.setVisible(true);
+        fenster.setLayout(null);
+        
+        fragetext = new Label();
+        fragetext.setText("Hier steht später die Frage und ggf. die Antworten.");
+        fragetext.setVisible(true);
+        fragetext.setLocation(200,50);
+        fragetext.setSize(400,50);
+        fenster.add(fragetext);
+        
+      
+    }  
+ 
+}
